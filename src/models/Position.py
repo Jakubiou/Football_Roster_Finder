113a70a2146b7e7834c72e5906d370d498e5a657
Šrámek <jakubsramek2006@gmail.com>
2025-12-31 15:58:58 +0100

    Added class position which is player position in football

---
+++ src/models/Position.py
@@ -0,0 +1,8 @@
+class Position:
+
+    def __init__(self, id=None, name=""):
+        self.id = id
+        self.name = name
+
+    def __repr__(self):
+        return f"Position(id={self.id}, name='{self.name}')"
