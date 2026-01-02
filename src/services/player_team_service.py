8e2c6d5b1d275a9cb443c4f4d7775bbe5cb39c3f
Šrámek <jakubsramek2006@gmail.com>
2026-01-02 20:50:46 +0100

    New module that create and update PlayerTeam table

---
+++ src/services/player_team_service.py
@@ -0,0 +1,32 @@
+def add_player_to_team(db, player_id, team_id, position_id):
+    sql = """
+    INSERT INTO PlayerTeam (player_id, team_id, position_id, minutes_played)
+    VALUES (?, ?, ?, 0)
+    """
+    db.execute(sql, player_id, team_id, position_id)
+    db.commit()
+
+
+def remove_player_from_team(db, player_id):
+    sql = "DELETE FROM PlayerTeam WHERE player_id = ?"
+    db.execute(sql, player_id)
+    db.commit()
+
+
+def update_minutes(db, player_id, team_id, minutes):
+    db.execute("""
+          UPDATE PlayerTeam
+          SET minutes_played = minutes_played + ?
+          WHERE player_id = ? AND team_id = ?
+      """, minutes, player_id, team_id)
+    db.commit()
+
+
+def change_position(db, player_id, new_position_id):
+    sql = """
+    UPDATE PlayerTeam
+    SET position_id = ?
+    WHERE player_id = ?
+    """
+    db.execute(sql, new_position_id, player_id)
+    db.commit()
