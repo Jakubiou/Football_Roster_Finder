d5aee1061623f03731873cafce128bdd5bef29b0
Šrámek <jakubsramek2006@gmail.com>
2026-01-11 16:28:24 +0100

    used own exceptions

--- src/db/Database.py
+++ src/db/Database.py
@@ -1,20 +1,18 @@
-import json
 import pyodbc
-import os
+from src.db.db_exceptions import ConfigError, DatabaseConnectionError
+from src.lib.config_loader import load_config
 
 
 class Database:
 
     def __init__(self, config_path="config.json"):
-        if not os.path.exists(config_path):
-            raise FileNotFoundError(f"Config soubor '{config_path}' nenalezen")
-
-        with open(config_path, "r", encoding="utf-8") as f:
-            config = json.load(f)
+        config = load_config(config_path)
 
         self.conn_str = config.get("connectionString")
         if not self.conn_str:
-            raise KeyError("Missing 'connectionString' in config.json")
+            raise ConfigError(
+                "V config.json chybí položka 'connectionString'."
+            )
 
         self.connection = None
         self.cursor = None
@@ -23,10 +21,11 @@ class Database:
         try:
             self.connection = pyodbc.connect(self.conn_str)
             self.cursor = self.connection.cursor()
-            print("Připojeno k databázi")
-        except pyodbc.Error as e:
-            print(f"Chyba připojení: {e}")
-            raise
+        except pyodbc.Error:
+            raise DatabaseConnectionError(
+                "Nepodařilo se připojit k databázi.\n"
+                "Zkontrolujte přihlašovací údaje a dostupnost serveru."
+            )
 
     def disconnect(self):
         if self.cursor:
